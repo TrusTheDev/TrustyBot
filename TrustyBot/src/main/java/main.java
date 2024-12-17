5c4c29546146ea56054545be34cf9055474cb834
Trusty <123142834+IvanViegas@users.noreply.github.com>
2024-12-17 20:36:28 -0300

    Project structure

---
+++ TrustyBot/src/main/java/main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        String option = "-1";
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Welcome to TrustyBot! /n 0: Cerrar /n 1: iniciar aplicacion /n");
+            option = sc.nextLine();
+            switch (option) {
+                case "0":
+                    System.exit(0);
+
+                case "1":
+                    System.out.println("1: secuencial mode (safe) /n 2: paralelism mode (unsafe) ");
+                    option = sc.nextLine();
+                    if(option == "1"){
+                        //secuencial mode
+                    }
+                    else if(option == "2"){
+                        //paralelism mode
+                    }
+                    break;
+            }
+        }
+    }
+}
