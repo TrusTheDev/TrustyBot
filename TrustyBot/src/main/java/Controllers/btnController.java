e35f2d01a902076fb5ea7ac7a02baab23f155124
Trusty <123142834+IvanViegas@users.noreply.github.com>
2024-12-20 18:49:07 -0300

    Add key

--- TrustyBot/src/main/java/Controllers/btnController.java
+++ TrustyBot/src/main/java/Controllers/btnController.java
@@ -4,13 +4,27 @@ import Models.KeyBtn;
 import repository.keycodeRepository;
 
 import java.awt.*;
+import java.util.Scanner;
 
 public class btnController {
-
+    Scanner sc = new Scanner(System.in);
     public void initProgram() throws AWTException, InterruptedException {}
 
     public void addKey(){
         System.out.println("1: Ingresar teclas 2: editar teclas");
+        String option = sc.nextLine();
+        if(option == "1"){
+            ();
+        }
+    }
+
+    public KeyBtn putkey(){
+        KeyBtn key = new KeyBtn(1000, 0, 5000);
+        System.out.println("Ingresar tiempo de presionado en milisegundos, default: " + key.getPressTime() + ", (" + key.getPressTime()/1000 );
+        key.setPressTime(sc.nextInt());
+        System.out.println("Ingresar tecla que desea presionar");
         
     }
+
+
 }
